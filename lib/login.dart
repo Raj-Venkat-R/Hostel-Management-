22879f14a63df99d94253befc4d894ea1766c99e
Raj-Venkat-R <rajvenkat.r361@gmail.com>
2025-08-27 19:32:24 +0530

    Partially Completed Login

--- lib/login.dart
+++ lib/login.dart
@@ -1,17 +1,141 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-class Login extends StatefulWidget {
-  const Login({super.key});
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
 
   @override
-  State<Login> createState() => _LoginState();
+  State<LoginScreen> createState() => _LoginScreenState();
 }
 
-class _LoginState extends State<Login> {
+class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Login Here",
+              style: GoogleFonts.exo2(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 2,
+                color: Colors.black,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                top: 20,
+                left: 50,
+                right: 50,
+                bottom: 20,
+              ),
+              child: Divider(color: Colors.black, thickness: 2),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 50, right: 50),
+              child: TextField(
+                controller: _usernameController,
+                style: GoogleFonts.exo2(fontSize: 14, color: Colors.black),
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  labelText: 'Username or Email',
+                  labelStyle: GoogleFonts.exo2(
+                    fontSize: 16,
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  hintText: 'Enter your username or email',
+                  hintStyle: GoogleFonts.exo2(
+                    fontSize: 14,
+                    color: Colors.black,
+                  ),
+                  hoverColor: Colors.black,
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: const BorderSide(color: Colors.black),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.only(left: 50, right: 50),
+              child: TextField(
+                controller: _passwordController,
+                style: GoogleFonts.exo2(fontSize: 14, color: Colors.black),
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  labelText: 'Password',
+                  labelStyle: GoogleFonts.exo2(
+                    fontSize: 16,
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  hintText: 'Enter your password',
+                  hintStyle: GoogleFonts.exo2(
+                    fontSize: 14,
+                    color: Colors.black,
+                  ),
+                  hoverColor: Colors.black,
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: const BorderSide(color: Colors.black),
+                  ),
+                  suffixIcon: const Icon(Icons.visibility, color: Colors.black),
+                ),
+              ),
+            ),
+            SizedBox(height: 30),
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.black,
+                padding: EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+              child: Text(
+                'Login',
+                style: GoogleFonts.exo2(
+                  fontSize: 16,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.black,
+                padding: EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+              child: Text(
+                'Forgot Password?',
+                style: GoogleFonts.exo2(
+                  fontSize: 12,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:hostel_management/login.dart';
 import 'firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
+
 void main() async {
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(HostelManagement());
 }
 
 class HostelManagement extends StatelessWidget {
@@ -13,8 +13,6 @@ class HostelManagement extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Login(),
-    );
+    return MaterialApp(home: LoginScreen());
   }
 }
